89bc793c40e746d8e03617c694556476237a4c0b
Maxton Wilson <maxtonwilson@gmail.com>
2024-11-24 15:47:14 -0600

    Added small class to receive coords

---
+++ receive_coords.py
@@ -0,0 +1,20 @@
+import serial
+
+class ReceiveCoords:
+    def __init__(self):
+        self.port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
+        self.lat = 0.0
+        self.lon = 0.0
+        self.alt = 0.0
+        self.roll = 0.0
+        self.pitch = 0.0
+        self.yaw = 0.9
+
+    def get_coords(self):
+        rcv = self.port.read()
+        # port.write("\r\nYou sent:" + repr(rcv))
+        self.lat, self.lon, self.alt, self.roll, self.pitch, self.yaw = repr(rcv).split(',') # lat, lon, alt, roll, pitch, yaw
+        return [self.lat, self.lon, self.alt, self.roll, self.pitch, self.yaw]
+
+    def get_lat_lon_yaw(self):
+        return [self.lat, self.lon, self.yaw]
