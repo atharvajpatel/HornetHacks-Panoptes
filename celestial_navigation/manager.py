17b5285ac3d391316474b82c6a2752c7343f37d3
Yoyo <52893812+exanova-y@users.noreply.github.com>
2024-11-24 01:14:49 -0800

    update

--- celestial_navigation/manager.py
+++ celestial_navigation/manager.py
@@ -1,6 +1,13 @@
 import star_detection as sd
 import star_matching as sm
 
+# solving problems:
+# use conda base python interpreter
+# deactivate and reactivate
+# also just close and open again after installation
+
+
+
 # image_with_stars, stars = sd.detection(example_path)
 # sgr = sm.Sagittarius()
 # sm.draw_constellation_on_image(image_with_stars, stars, sgr)
